3fce70809bffd55a711cb47cafe4781039de7b93
ErikKarlkvist <erikkarlkvist95@gmail.com>
2015-09-21 16:38:28 +0200

    login screen

---
+++ EcoBussing/app/src/main/java/com/example/spoti5/ecobussing/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.spoti5.ecobussing;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by erikk on 2015-09-21.
+ */
+public class LoginActivity extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+    }
+}
