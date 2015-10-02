4e87592e12da9211d8307ecbf89fb273fd7db264
ClausX <h_ronstrom@hotmail.com>
2015-10-02 22:37:26 +0200

    added some classes for the Json returned by Googles Directions api

---
+++ EcoBussing/app/src/main/java/com/example/spoti5/ecobussing/JsonClasses/Directions/StartLocation.java
@@ -0,0 +1,18 @@
+package com.example.spoti5.ecobussing.JsonClasses.Directions;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Hampus on 2015-10-02.
+ */
+public class StartLocation {
+
+    @SerializedName("lat")
+    @Expose
+    public Double lat;
+    @SerializedName("lng")
+    @Expose
+    public Double lng;
+
+}
