3798d37d2db982ebf09047d73276087ec960e646
ClausX <h_ronstrom@hotmail.com>
2015-10-02 23:59:14 +0200

    variables and getters in BusStop

--- EcoBussing/app/src/main/java/com/example/spoti5/ecobussing/BusData/BusStop.java
+++ EcoBussing/app/src/main/java/com/example/spoti5/ecobussing/BusData/BusStop.java
@@ -4,4 +4,17 @@ package com.example.spoti5.ecobussing.BusData;
  * Created by Hampus on 2015-10-02.
  */
 public class BusStop {
+    private String name;
+    private double lat;
+    private double lng;
+
+    public BusStop(String name, double lat, double lng){
+        this.name = name;
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+    public String getName(){return name;}
+    public double getLat(){return lat;}
+    public double getLng(){return lng;}
 }
