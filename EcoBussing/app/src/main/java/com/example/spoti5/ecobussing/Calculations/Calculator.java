a30b05c1a967768f8f6a6e7bf64c913994143876
ClausX <h_ronstrom@hotmail.com>
2015-10-02 23:14:03 +0200

    can now calculate driving distance between two positions

--- EcoBussing/app/src/main/java/com/example/spoti5/ecobussing/Calculations/Calculator.java
+++ EcoBussing/app/src/main/java/com/example/spoti5/ecobussing/Calculations/Calculator.java
@@ -1,6 +1,14 @@
 package com.example.spoti5.ecobussing.Calculations;
 
+import com.example.spoti5.ecobussing.JsonClasses.Directions.Directions;
 import com.example.spoti5.ecobussing.SavedData.SaveHandler;
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
 
 /**
  * Created by hilden on 2015-09-16.
@@ -13,6 +21,8 @@ public class Calculator {
     private double petrolPrice = 14.32;
     private double carbondioxideEmittedPerLiter = 2.348; //Currently measured in grams.
 
+    private String baseURL = "https://maps.googleapis.com/maps/api/directions/json?";
+
     public static Calculator getCalculator() {
         if (calculator == null) {
             calculator = new Calculator();
@@ -20,7 +30,45 @@ public class Calculator {
         return calculator;
     }
 
-    public void calculateDistance() {}
+    public int calculateDistance(double startLat, double startLng, double endLat, double endLng,
+                                    String key) {
+        int dist = 0;
+
+        String completeURL = baseURL + "origin=" + startLat + "," + startLng + "&destination=" +
+                endLat + "," + endLng +"&key=" + key;
+
+        try{
+            String jsonString = readUrl(completeURL);
+            Gson gson = new Gson();
+            Directions directions = gson.fromJson(jsonString, Directions.class);
+            dist = directions.routes.get(0).legs.get(0).distance.value;
+        }catch(IOException e){
+            System.out.println(e);
+        }catch (Exception e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+
+        return dist;
+    }
+
+    private static String readUrl(String urlString) throws Exception {
+        BufferedReader reader = null;
+        try {
+            URL url = new URL(urlString);
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuffer buffer = new StringBuffer();
+            int read;
+            char[] chars = new char[1024];
+            while ((read = reader.read(chars)) != -1)
+                buffer.append(chars, 0, read);
+
+            return buffer.toString();
+        } finally {
+            if (reader != null)
+                reader.close();
+        }
+    }
 
     /**
      * @returns the amount of money saved since last startup in KR.
