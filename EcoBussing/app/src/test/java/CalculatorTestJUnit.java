c725185506633a02c16d2492b94691d1764e52be
ClausX <h_ronstrom@hotmail.com>
2015-10-11 21:31:38 +0200

    added JUnit test for calculator

---
+++ EcoBussing/app/src/test/java/CalculatorTestJUnit.java
@@ -0,0 +1,19 @@
+import junit.framework.TestCase;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Hampus on 2015-10-11.
+ */
+public class CalculatorTestJUnit extends TestCase {
+
+    @org.junit.Test
+    public void testCalculateDistance() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testCalculateDistance1() throws Exception {
+
+    }
+}
