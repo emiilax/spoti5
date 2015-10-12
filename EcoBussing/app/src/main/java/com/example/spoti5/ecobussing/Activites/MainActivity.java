53e18329758462a7aea7d1134f98c872c3965a87
ClausX <h_ronstrom@hotmail.com>
2015-10-12 17:12:22 +0200

    Changed ProfileInfo to contain the code of the old ProfileFragment

--- EcoBussing/app/src/main/java/com/example/spoti5/ecobussing/Activites/MainActivity.java
+++ EcoBussing/app/src/main/java/com/example/spoti5/ecobussing/Activites/MainActivity.java
@@ -20,9 +20,7 @@ import android.support.v7.widget.Toolbar;
 
 import com.example.spoti5.ecobussing.BusinessFragment;
 import com.example.spoti5.ecobussing.CreateCompanyFragment;
-import com.example.spoti5.ecobussing.EditProfile.EditInfoFragment;
 import com.example.spoti5.ecobussing.EditProfile.ProfileSwipeFragment;
-import com.example.spoti5.ecobussing.ProfileFragment;
 import com.example.spoti5.ecobussing.Profiles.ProfileView;
 import com.example.spoti5.ecobussing.R;
 import com.example.spoti5.ecobussing.SavedData.SaveHandler;
