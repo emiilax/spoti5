ac83ec95b8c55cba2218b0e4c16402da6e082b48
ClausX <h_ronstrom@hotmail.com>
2015-10-12 23:02:37 +0200

    added some placeholder images and text in profile with info

--- EcoBussing/app/src/main/java/com/example/spoti5/ecobussing/Profiles/UserProfileView.java
+++ EcoBussing/app/src/main/java/com/example/spoti5/ecobussing/Profiles/UserProfileView.java
@@ -33,7 +33,7 @@ public class UserProfileView extends Fragment implements ViewPager.OnPageChangeL
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        view = inflater.inflate(R.layout.fragment_profile_view, container, false);
+        view = inflater.inflate(R.layout.fragment_user_profile_view, container, false);
 
         mPagerAdapter = new ProfilePagerAdapter(getActivity().getSupportFragmentManager());
 
