4fd2452608a108b40610e549c75bda6798935772
ClausX <h_ronstrom@hotmail.com>
2015-10-27 12:08:47 +0100

    added ElectricityApiJUnit to AllJUnitTests

--- EcoBussing/app/src/test/java/com/example/spoti5/ecobussing/AllJUnitTests.java
+++ EcoBussing/app/src/test/java/com/example/spoti5/ecobussing/AllJUnitTests.java
@@ -1,5 +1,6 @@
 package com.example.spoti5.ecobussing;
 
+import com.example.spoti5.ecobussing.apirequests.ElectricityApiJUnit;
 import com.example.spoti5.ecobussing.calculations.CalculatorTestJUnit;
 
 import org.junit.runner.RunWith;
@@ -9,6 +10,6 @@ import org.junit.runners.Suite;
  * Created by Hampus on 2015-10-13.
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({CalculatorTestJUnit.class})
+@Suite.SuiteClasses({CalculatorTestJUnit.class, ElectricityApiJUnit.class})
 public class AllJUnitTests {
 }
