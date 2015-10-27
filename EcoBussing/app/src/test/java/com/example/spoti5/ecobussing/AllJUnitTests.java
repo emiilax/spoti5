abd961100619f63f6b3dfef626835de69f310a58
ClausX <h_ronstrom@hotmail.com>
2015-10-27 19:20:38 +0100

    added UserTestJUnit to AllJUnitTests

--- EcoBussing/app/src/test/java/com/example/spoti5/ecobussing/AllJUnitTests.java
+++ EcoBussing/app/src/test/java/com/example/spoti5/ecobussing/AllJUnitTests.java
@@ -2,6 +2,7 @@ package com.example.spoti5.ecobussing;
 
 import com.example.spoti5.ecobussing.apirequests.ElectricityApiJUnit;
 import com.example.spoti5.ecobussing.calculations.CalculatorTestJUnit;
+import com.example.spoti5.ecobussing.model.profile.UserTestJUnit;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -10,6 +11,6 @@ import org.junit.runners.Suite;
  * Created by Hampus on 2015-10-13.
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({CalculatorTestJUnit.class, ElectricityApiJUnit.class})
+@Suite.SuiteClasses({CalculatorTestJUnit.class, ElectricityApiJUnit.class, UserTestJUnit.class})
 public class AllJUnitTests {
 }
