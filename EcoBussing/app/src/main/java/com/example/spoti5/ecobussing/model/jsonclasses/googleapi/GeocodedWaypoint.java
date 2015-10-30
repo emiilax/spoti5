e753134abc944aa8e36ea0a4885b9ab645f48aa2
ClausX <h_ronstrom@hotmail.com>
2015-10-30 11:28:28 +0100

    fixed some tests and some comments

--- EcoBussing/app/src/main/java/com/example/spoti5/ecobussing/model/jsonclasses/googleapi/GeocodedWaypoint.java
+++ EcoBussing/app/src/main/java/com/example/spoti5/ecobussing/model/jsonclasses/googleapi/GeocodedWaypoint.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Theses are classes that are used to store the data gotten from the Google Maps Directions API. The
+ * classes were generated using http://www.jsonschema2pojo.org/ before they were moderated, or rather
+ * stripped down, since there was no use for a large portion of the Google Maps data.
  * Created by Hampus on 2015-10-02.
  */
 public class GeocodedWaypoint {
--- EcoBussing/app/src/test/java/com/example/spoti5/ecobussing/AllJUnitTests.java
+++ EcoBussing/app/src/test/java/com/example/spoti5/ecobussing/AllJUnitTests.java
@@ -4,6 +4,7 @@ import com.example.spoti5.ecobussing.apirequests.ElectricityApiJUnit;
 import com.example.spoti5.ecobussing.controller.calculations.CalculatorTestJUnit;
 import com.example.spoti5.ecobussing.controller.calculations.CheckUserInputTestJUnit;
 import com.example.spoti5.ecobussing.model.profile.CompanyTestJUnit;
+import com.example.spoti5.ecobussing.model.profile.DeepMapTestJUnit;
 import com.example.spoti5.ecobussing.model.profile.UserTestJUnit;
 
 import org.junit.runner.RunWith;
@@ -14,6 +15,6 @@ import org.junit.runners.Suite;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({CalculatorTestJUnit.class, ElectricityApiJUnit.class, UserTestJUnit.class,
-        CheckUserInputTestJUnit.class, CompanyTestJUnit.class})
+        CheckUserInputTestJUnit.class, CompanyTestJUnit.class, DeepMapTestJUnit.class})
 public class AllJUnitTests {
 }
